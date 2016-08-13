40f183d3f8ea534515603fa6e2f5c90dc037a0a5
Adrien Pellerin <adrienip@gmail.com>
2016-08-12 20:25:59 -0400

    setup isotope

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 $(function() {
+  var filter_values = {
+    'all': "*",
+    'shows': ".tag-show",
+    'videos': ".tag-video"
+  };
 
+  var $iso_grid = $('.iso-grid');
+
+  $iso_grid.isotope({
+    itemSelector: '.iso-grid-item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.iso-grid-sizer'
+    }
+  });
+
+  $('.iso-filter').on('click', function(e) {
+    var $filter = $(this);
+    var filter_value = filter_values[$filter.data('filter')];
+
+    $iso_grid.isotope({
+      filter: filter_value
+    });
+
+    var $filter_group = $filter.closest('.iso-filter-group');
+    $filter_group.find('.iso-filter').removeClass('active');
+    $filter.addClass('active');
+  });
 });
